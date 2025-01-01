68940f195156720edae5cde163bed3461029a8cd
shekhar <reach2shekhar@gmail.com>
2025-01-01 12:30:36 +0530

    added gemini1.5 flash model

--- chattest.py
+++ chattest.py
@@ -7,7 +7,7 @@ API_KEY = os.getenv("API_KEY")
 genai.configure(api_key=API_KEY)
 
 # Initialize the model
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-1.5-flash")
 chat = model.start_chat(
     history=[
         {"role": "user", "parts": "Hello AI"},
@@ -16,7 +16,7 @@ chat = model.start_chat(
 )
 
 # Streamlit app interface
-st.title("Chat with Shekhar AI..powered by gemini-pro")
+st.title("Chat with Shekhar AI..powered by gemini-1.5-flash")
 st.write("Type your message below:")
 
 # Initialize the session state for storing chat history
@@ -30,14 +30,15 @@ for message in st.session_state.history:
     role = message["role"]
     text = message["parts"]
     if role == "user":
+        with st.chat_message("user"):
+            st.markdown(text)
         # User message styled in black
-        st.markdown(f'<p style="color:black;"><b>You</b>: {text}</p>', unsafe_allow_html=True)
+        # st.markdown(f'<p style="color:black;"><b>You</b>: {text}</p>', unsafe_allow_html=True)
     else:
-        # AI response styled in blue
-        st.markdown(f'<p style="color:blue;"><b>AI</b>: {text}</p>', unsafe_allow_html=True)
-
+        with st.chat_message("assistant"):
+            st.markdown(text)
 # Input box for user message
-user_input = st.text_input("Your message:")
+user_input = st.text_area("Your message:")
 
 # Send button to process the message
 if st.button("Send"):
